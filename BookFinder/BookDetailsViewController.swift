652ce32bf272f6bdef03cf4525260f204ba08ef5
Atikur Rahman <atik.bd08@gmail.com>
2016-06-19 16:42:14 +0600

    show book details

--- BookFinder/BookDetailsViewController.swift
+++ BookFinder/BookDetailsViewController.swift
@@ -10,6 +10,37 @@ import UIKit
 
 class BookDetailsViewController: UIViewController {
     
+    @IBOutlet weak var bookImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var genreLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    
     var book: Book!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.titleLabel.text = book.name
+        self.authorLabel.text = "Author: \(book.artist)"
+        self.genreLabel.text = "Genre: \(book.genres)"
+        self.priceLabel.text = "Price: \(book.price) \(book.currency)"
+        
+        downloadBookCoverImage()
+    }
+    
+    func downloadBookCoverImage() {
+        ITCClient.sharedInstance.taskForImageWithUrl(book.imageUrl100) {
+            data, error in
+            
+            guard let data = data,
+                downloadedImage = UIImage(data: data) else {
+                    return
+            }
+            
+            dispatch_async(dispatch_get_main_queue()) {
+                self.bookImageView.image = downloadedImage
+            }
+        }
+    }
 }
